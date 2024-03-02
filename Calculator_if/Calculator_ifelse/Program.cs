c1fbd18d58c0154e68277f64c7e3b0add86bd931
Hamidovaa <hemidoffa55@gmail.com>
2024-03-03 02:01:17 +0400

    commit

---
+++ Calculator_if/Calculator_ifelse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculator");
+            Console.WriteLine("Birinci eded:");
+            int number1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci eded:");
+            int number2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("emeliyyat");
+            Console.WriteLine("+, -, /, *");
+            char simvol = Convert.ToChar(Console.ReadLine());
+            if(simvol == '+')
+            {
+                int a= number1 + number2;
+                Console.WriteLine("cemi:" + a);
+            }
+            if (simvol == '-')
+            {
+                int b = number1 - number2;
+                Console.WriteLine("ferqi:" + b);
+            }
+            if (simvol == '*')
+            {
+                int c = number1 * number2;
+                Console.WriteLine("hasil:" + c);
+            }
+            if (simvol == '+')
+            {
+                int d = number1 / number2;
+                Console.WriteLine("qismet:" + d);
+            }
+
+        }
+    }
+}
