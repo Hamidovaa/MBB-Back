9234ec9d347d64fa4deaa1b909c40594102fbef4
Hamidovaa <hemidoffa55@gmail.com>
2024-03-03 21:19:48 +0400

    Calculator task

---
+++ Lesson7task/Lesson7task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci ededi daxil edin:");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emeliyyat daxil edin:");
+            char option=Convert.ToChar(Console.ReadLine());
+
+            if(option == '+')
+            {
+                Console.WriteLine(x1 + x2);
+            }
+            else if(option == '-')
+            {
+                Console.WriteLine(x1- x2);
+            }
+            else if(option== '*')
+            {
+                Console.WriteLine(x1 * x2);
+            }
+            else if(option=='/')
+            {
+                Console.WriteLine(x1 / x2);
+            }
+            else
+            {
+                Console.WriteLine("sehv emeliyyat");
+            }
+        }
+    }
+}
